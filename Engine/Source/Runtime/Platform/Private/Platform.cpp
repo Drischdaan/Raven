1b8c06d2f40ae5a32a62c946bd7418ceafae9876
Drischdaan <drischdaan@protonmail.com>
2026-07-16 13:18:27 +0200

    refactor(platform): remove glfw windowing from platform

--- Engine/Source/Runtime/Platform/Private/Platform.cpp
+++ Engine/Source/Runtime/Platform/Private/Platform.cpp
@@ -2,26 +2,14 @@
 
 #include "Platform.hpp"
 
-#include "Window_Default.hpp"
-
-#include "GLFW/glfw3.h"
-
 void FPlatform::Initialize()
 {
-    glfwInit();
 }
 
 void FPlatform::Update()
 {
-    glfwPollEvents();
 }
 
 void FPlatform::Shutdown()
 {
-    glfwTerminate();
-}
-
-IWindow* FPlatform::CreateWindow(const FWindowProperties& Properties)
-{
-    return new FWindow_Default(Properties);
 }
--- Engine/Source/Runtime/Platform/Public/Platform.hpp
+++ Engine/Source/Runtime/Platform/Public/Platform.hpp
@@ -1,7 +1,6 @@
 // Copyright RavenStormStudio 2026 All Rights Reserved Copyright
 
 #pragma once
-#include "Window.hpp"
 
 class PLATFORM_API FPlatform
 {
@@ -9,8 +8,6 @@ public:
     static void Initialize();
     static void Update();
     static void Shutdown();
-
-    [[nodiscard]] static IWindow* CreateWindow(const FWindowProperties& Properties);
 
 private:
 };
