2324c0bc8f4d4d3b9b66c52d08eb0e8079086e62
Drischdaan <drischdaan@protonmail.com>
2026-07-16 13:17:10 +0200

    feat(core): add atomic type aliase

---
+++ Engine/Source/Runtime/Core/Public/Utility/Atomic.hpp
@@ -0,0 +1,10 @@
+﻿// Copyright RavenStormStudio 2026 All Rights Reserved Copyright
+
+#pragma once
+
+#include <atomic>
+
+template <typename T>
+using TAtomic = std::atomic<T>;
+
+using FAtomicFlag = std::atomic_flag;
